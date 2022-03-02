8b3959a91679e8a81992b4226185a617d286ac76
Stephen Olayemi <olayemistephen007@gmail.com>
2022-03-02 17:07:00 +0100

    Changes in prgress

--- src/screens/TextScreen.js
+++ src/screens/TextScreen.js
@@ -1,14 +1,21 @@
-import React from 'react'
+import React, { useState } from 'react'
 import { View, Text, StyleSheet, TextInput } from 'react-native'
 
 const TextScreen = () => {
+
+    const [name, setName] = useState('')
+
     return (
         <View>
+            <Text>Enter name:</Text>
             <TextInput 
                 style={styles.input}
                 autoCapitalize = "none"
                 autoCorrect= {false}
+                value={name}
+                onChangeText={(val) => setName(val)}
             />
+            <Text>My name is {name}</Text>
         </View>
     )
 }
